c07621f2c1ab546c1f43b3afb871b75d9b9206dd
Shobhit Mishra <shmishra@microsoft.com>
2019-12-30 09:18:37 -0800

    Added validation

---
+++ ClosestPrime/ClosestPrime/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ClosestPrime.Models;
+
+namespace ClosestPrime.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checknumber(NumberInput number)
+        {
+            if (!ModelState.IsValid)
+                ModelState.AddModelError(nameof(NumberInput),
+                    "The input has to be a 'number' less than 179424692.");
+            return View("Index", number);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ClosestPrime/ClosestPrime/Models/NumberInput.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace ClosestPrime.Models
+{
+    public class NumberInput
+    {
+        [Required]
+        [ValidInputNumber(ErrorMessage = "You must enter a number less than 179424692")]
+        public int Input { get; set; }
+    }
+
+    public class ValidInputNumber : Attribute, IModelValidator 
+    {
+        public string ErrorMessage { get; set; } = "Input has to be a number less than 179424692";
+
+        public IEnumerable<ModelValidationResult> Validate(ModelValidationContext context)
+        {            
+            var inputNumer = context.Model as int?;            
+            if (!inputNumer.HasValue || inputNumer >= 179424692)
+            {
+                return new List<ModelValidationResult>
+                {
+                    new ModelValidationResult("", ErrorMessage)
+                };
+            }
+            else
+            {
+                return Enumerable.Empty<ModelValidationResult>();
+            }
+        }
+    }
+}
