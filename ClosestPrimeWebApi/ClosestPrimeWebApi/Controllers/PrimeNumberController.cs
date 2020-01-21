07ab79d19297c3df6b4b87dd3db931153e70f7c3
Shobhit Mishra <shmishra@microsoft.com>
2020-01-20 21:02:48 -0800

    Implemented the webapi solution

---
+++ ClosestPrimeWebApi/ClosestPrimeWebApi/Controllers/PrimeNumberController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClosestPrimeWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ClosestPrimeWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PrimeNumberController : Controller
+    {
+        private int GetClosestPrime(int number)
+        {
+            var primes = PrimeNumberRepository.Primes;
+            if (primes.Contains(number))
+                return number;
+            int i = 1;
+            while (true)
+            {
+                if (primes.Contains(number - i))
+                    return number - i;
+                if (primes.Contains(number + i))
+                    return number + i;
+                i++;
+            }
+        }
+
+        // GET: api/<controller>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        
+
+        // POST api/<controller>
+        [HttpPost]
+        public int Post([FromBody]int number)
+        {
+            var closestPrime = GetClosestPrime(number);            
+            return closestPrime;
+        }
+        
+        // GET api/<controller>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        // PUT api/<controller>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE api/<controller>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ ClosestPrimeWebApi/ClosestPrimeWebApi/wwwroot/js/site.js
@@ -0,0 +1,24 @@
+﻿const uri = 'api/PrimeNumber';
+let todos = [];
+
+function getClosestPrime() {
+    var number = document.getElementById('input-number').value;
+    fetch(uri, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: number
+    })
+        .then(response => response.json())
+        .then((closestPrime) => _displayClosestPrime(number, closestPrime))
+        .catch(error => console.error('Unable to get prime.', error));
+}
+
+function _displayClosestPrime(number, closestPrime) {
+    const messageElement = document.getElementById('prime-message');
+    var message = `Closest prime to ${number} is ${closestPrime}.`
+    messageElement.innerHTML = `<p>${message}</p>`
+}
+
