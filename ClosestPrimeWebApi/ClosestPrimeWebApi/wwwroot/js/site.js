76865d5bcd31413cd7893dbe78921af71f2d32d0
Shobhit Mishra <shmishra@microsoft.com>
2020-02-04 21:32:19 -0800

    Added the getting of all number

--- ClosestPrimeWebApi/ClosestPrimeWebApi/wwwroot/js/site.js
+++ ClosestPrimeWebApi/ClosestPrimeWebApi/wwwroot/js/site.js
@@ -16,15 +16,6 @@ function getClosestPrime() {
         .catch(error => console.error('Unable to get prime.', error));
 }
 
-function getMaximumDifference() {
-    fetch(uri, {
-        method: 'GET'           
-    })
-        .then(response => response.json())
-        .then((maxDifferenceEntity) => _displayMaximumDifference(maxDifferenceEntity))
-        .catch(error => console.error('Unable to get maximum difference.', error));
-}
-
 function _displayClosestPrime(number, numberEntity) {
     const messageElement = document.getElementById('prime-message');
     var message = `Closest prime to ${number} is ${numberEntity.matchedPrime}. It has been entered ${numberEntity.count} times.`
@@ -45,6 +36,16 @@ function _displayClosestPrime(number, numberEntity) {
 
     messageElement.innerHTML = `<p>${message}</p>`;
     getMaximumDifference();
+    getAndShowAllNumbersSoFar();
+}
+
+function getMaximumDifference() {
+    fetch(uri, {
+        method: 'GET'
+    })
+        .then(response => response.json())
+        .then((maxDifferenceEntity) => _displayMaximumDifference(maxDifferenceEntity))
+        .catch(error => console.error('Unable to get maximum difference.', error));
 }
 
 function _displayMaximumDifference(maxDifferenceEntity) {
@@ -54,3 +55,32 @@ function _displayMaximumDifference(maxDifferenceEntity) {
     //  msg = `Highest difference found so far is also not that huge.<br> It is just ${maxDifferenceEntity.difference}. <br>  It was discovered when someone entered ${maxDifferenceEntity.number}. Closest prime was ${maxDifferenceEntity.matchedPrime}. <br> It has occured ${maxDifferenceEntity.count} times.`
     maxDiffMessageElement.innerHTML = `<p>${msg}</p>`
 }
+
+function getAndShowAllNumbersSoFar() {
+    var getAllUri = `${uri}/getall`;
+    fetch(getAllUri, {
+        method: 'GET'
+    })
+        .then(response => response.json())
+        .then((allnumbers) => _displayAllNumbers(allnumbers))
+        .catch(error => console.error('Unable to get numbers.', error));
+}
+
+function _displayAllNumbers(allNumberEntities) {
+    // Create a div and add the list here
+    const allNumsElement = document.createElement('div');
+    
+    var unorderedList = document.createElement('ul');    
+    for(var i = 0; i < allNumberEntities.length; i++)
+    {
+        var num = allNumberEntities[i];
+        var listItem = document.createElement('li');
+        var listText = document.createTextNode(`number: ${num.number}, matched prime: ${num.matchedPrime} and count: ${num.count}`);
+        listItem.appendChild(listText);      
+        unorderedList.appendChild(listItem);
+    }
+
+    allNumsElement.appendChild(unorderedList);
+    document.body.appendChild(allNumsElement);
+ }
+
